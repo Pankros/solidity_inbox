3b5ce9dfabffbdd30eb206eb218cd646893c4338
Pancho <master.rubio@gmail.com>
2022-01-04 12:12:44 +0100

    improve documentation

--- deploy.js
+++ deploy.js
@@ -1,4 +1,8 @@
 //MNEMONIC='<MNEMONIC>' URL_NETWORK='<https://rinkeby.infura.io/v3/a...>' node deploy.js
+//or
+//export MNEMONIC='<MNEMONIC>'
+//export URL_NETWORK='<URL_NETWORK>'
+//node deploy.js
 const MNEMONIC = process.env.MNEMONIC;
 const URL_NETWORK = process.env.URL_NETWORK;
 
